aa825c8c3033c5a7d84a2c46aed71832845b4214
Anna Moeller <am249534@CFCTROUT-LM22.local>
2023-07-14 14:49:59 -0500

    Clean data - all seasons

---
+++ R/Make clean deer and elk df.R
@@ -0,0 +1,30 @@
+# Make clean deer and elk data
+# Anna Moeller 
+# 7-14-23
+
+library(tidyverse)
+
+# data
+camfiles <- list.files("data/pictures by year", 
+                       full.names = TRUE)
+camraw_1 <- map_dfr(camfiles[c(1:3,5)], read_csv) %>% 
+  mutate(Folder = as.character(Folder))
+camraw_2 <- read_csv(camfiles[4])
+camraw <- bind_rows(camraw_1, camraw_2)
+
+# Clean up data types and dates (including assigning GMT time so that times
+# won't change for daylight savings)
+cam <- camraw %>%
+  mutate(datetime_chr = paste(Date, Time),
+         datetime = as.POSIXct(datetime_chr, format = "%d-%b-%y %H:%M:%S", tz = "GMT")) %>% 
+  # Get total count of elk per photo
+  mutate(ElkCount = ElkMatBull + ElkSpike + ElkCow + ElkCalf + ElkUnkn,
+         DeerCount = WTDbuck + WTDdoe + WTDfawn + WTDunkn) %>% 
+  rename(cam = CamID) %>% 
+  select(cam, File, Folder, datetime, ElkCount, DeerCount)
+
+saveRDS(cam, "data/clean/CleanElkandDeer.rds")
+
+
+
+
